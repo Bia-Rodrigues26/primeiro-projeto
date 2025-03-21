6b8ffc6096c3bdee635084d1635a332d718b0a53
Bia.Rodrigues26 <beatriz.rodrigues19@fatec.sp.gov.br>
2025-03-20 21:23:29 -0300

    Aula 20/03 as 21:23

---
+++ projeto2025/projeto2025/src/main/java/exercicios/Cliente.java
@@ -0,0 +1,68 @@
+package exercicios;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name="clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 60)
+    private String nome;
+    @Column()
+    private Integer idade;
+    @Column(nullable = false, length = 90)
+    private String endereco;
+
+    // Construtores
+    public Cliente() {}
+
+    public Cliente(Long id, String nome, int idade, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
